08853c22fd5bda6794bd31257a3d0edbca18cd0b
The_Baztyx <11617389+CypherNoodle@users.noreply.github.com>
2021-02-18 20:39:08 -0300

    Mejor modo serial
    Mayor claridad en modo serial para mejor visual en la consola.

--- Medicion_de_potencia/src/botones.h
+++ Medicion_de_potencia/src/botones.h
@@ -1,25 +1,26 @@
 void get_boton()
 {
 /* Inicio de configuracion del boton: */
-  buttonState = digitalRead(2);                      /* Leer la entrada del boton */
+  buttonState = digitalRead(2);                      /* Lee la entrada del boton */
   if (buttonState != lastButtonState) {              /* Compara el estado previo del boton */
-    if (buttonState == HIGH) {                       /* Si el estado cambio, incrementa el contador */
-      buttonPushCounter += 1;                        /* Si el estado actual es HIGH, entonces el boton va de off a on */
+    if (buttonState == HIGH) {                       /* Si el estado cambia, incrementa el contador */
+      buttonPushCounter += 1;                        /* Si el estado actual es HIGH, entonces va de off a on */
       Serial.println("Boton encendido");
+	    Serial.println(" ");
       Serial.print("numero de pulsaciones: ");
       Serial.println(buttonPushCounter);
-    } else {                                         /* Si el estado estado es LOW, entonces el boton va de on a off */
+    } else {                                         /* Si el estado estado es LOW, entonces va de on a off */
+	    Serial.println(" ");
       Serial.println("Boton apagado");
     }
-    delay(5);                                        /* Un poco de retraso para prevenir el efecto rebote del boton */
+    delay(5);                                        /* Retardo para prevenir el efecto rebote del boton */
   }
   lastButtonState = buttonState;                     /* Guarda el estado actual como ultimo estado conocido */
-  if (buttonPushCounter % 2 == 0) {                  /* Enciende el rele cada 2 pulsaciones ya que divide en 2 el estado actual */
-  digitalWrite(rele1, HIGH);                         /* Normanlmente abierto */
-  digitalWrite(rele2, LOW);                          /* Normanlmente cerrado */
+  if (buttonPushCounter % 2 == 0) {                  /* Enciende el rele 1 y apaga rele 2 */
+  digitalWrite(rele1, HIGH);                         
+  digitalWrite(rele2, LOW);                          
    } else {
-  digitalWrite(rele1, LOW);
+  digitalWrite(rele1, LOW);                          /* Enciende el rele 2 y apaga rele 1 */
   digitalWrite(rele2, HIGH);
   }
 }
-
